ca18690a119a6ecc6c600ab750dea55ab5f2214b
Mishalolka <krasava223a@gmail.com>
2023-11-17 14:07:00 +0200

    added lorem

--- my-app/src/components/HomePage/Home.tsx
+++ my-app/src/components/HomePage/Home.tsx
@@ -4,9 +4,25 @@ const Home = () => {
   return (
     <>
       <div>
-
         <div>Home</div>
-        <div>Home</div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+          laudantium, sed suscipit nobis, exercitationem qui libero iure
+          excepturi dolorem earum omnis, fugiat sint veritatis voluptatem
+          assumenda repellat ullam dicta labore.
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+          laudantium, sed suscipit nobis, exercitationem qui libero iure
+          excepturi dolorem earum omnis, fugiat sint veritatis voluptatem
+          assumenda repellat ullam dicta labore.
+        </div>
+        <div>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+          laudantium, sed suscipit nobis, exercitationem qui libero iure
+          excepturi dolorem earum omnis, fugiat sint veritatis voluptatem
+          assumenda repellat ullam dicta labore.
+        </div>
         <Link to={"products"}>to products</Link>
       </div>
     </>
