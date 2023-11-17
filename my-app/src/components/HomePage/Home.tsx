221f8d0d5270d494b693b728fbfc75dbdce68309
Mishalolka <krasava223a@gmail.com>
2023-11-17 14:07:47 +0200

    added Home divs

--- my-app/src/components/HomePage/Home.tsx
+++ my-app/src/components/HomePage/Home.tsx
@@ -7,6 +7,9 @@ const Home = () => {
 
         <div>Home</div>
         <div>Home</div>
+        <div>Home</div>
+        <div>Home</div>
+        <div>Home</div>
         <Link to={"products"}>to products</Link>
       </div>
     </>
