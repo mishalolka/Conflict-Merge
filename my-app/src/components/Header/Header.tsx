4340dd2c70deda716bd0cafb3ec67f19125df5f3
Mishalolka <krasava223a@gmail.com>
2023-11-19 23:26:30 +0200

    update desing

--- my-app/src/components/Header/Header.tsx
+++ my-app/src/components/Header/Header.tsx
@@ -1,12 +1,32 @@
+import { Link } from "react-router-dom";
+import styles from "./Header.module.css";
 
 const Header = () => {
-    return (
-      <>
-        <div>
-          <div>Header</div>
+  return (
+    <>
+      <nav className="navbar navbar-expand-lg">
+        <div className="container">
+          <div className="row">
+            <div className="col-3">
+              <Link className={"navbar-brand"} to="/">
+                <span className={styles.logo}>Party Time!</span>
+              </Link>
+            </div>
+            <div className="col-6">
+                <div className="col">Home</div>
+                <div className="col">Gallery</div>
+                <div className="col">About Party</div>
+                <div className="col">Reservation</div>
+                <div className="col">Contacts</div>
+            </div>
+            <div className="col-3">
+
+            </div>
+          </div>
         </div>
-      </>
-    );
-  };
-  
-  export default Header;
+      </nav>
+    </>
+  );
+};
+
+export default Header;
